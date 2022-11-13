f0e2833658d3d6c8b3a0f4ed4eb5040337eb9771
annaKumchar <anna.kumchar@gmail.com>
2022-11-13 22:16:54 +0200

    corrected

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,39 +1,46 @@
 import throttle from 'lodash.throttle';
 
 const formEl = document.querySelector('.feedback-form');
-const formEmailEl = document.querySelector('input');
-const formMessegeEl = document.querySelector('textarea');
 
 formEl.addEventListener('input', throttle(onFormFillingSet, 500));
 formEl.addEventListener('submit', onFormSubmit);
 
 const FORM_KEY = 'feedback-form-state';
-const formInputEl = {
-  email: '',
-  message: '',
-};
-function onFormFillingSet(event) {
-  const { name, value } = event.target;
 
+function onFormFillingSet(event) {
   let savedDate = JSON.parse(localStorage.getItem(FORM_KEY)) ?? {};
+
+  const { name, value } = event.target;
 
   savedDate = { ...savedDate, [name]: value };
 
-  localStorage.setItem('feedback-form-state', JSON.stringify(savedDate));
+  localStorage.setItem(FORM_KEY, JSON.stringify(savedDate));
 }
 
 function onFormSubmit(event) {
   event.preventDefault();
+
+  let finalData = {};
+  const formData = new FormData(formEl);
+  for (const [name, value] of formData.entries()) {
+    finalData[name] = value;
+  }
   event.currentTarget.reset();
-  console.log(formInputEl);
+  console.log(finalData);
 }
 
 function onFormFillingGet() {
-  const getValueLocalStorage = localStorage.getItem('feedback-form-state');
-  const parsedGetValueLocalStorage = JSON.parse(getValueLocalStorage);
-  if (parsedGetValueLocalStorage) {
-    formEmailEl.value = parsedGetValueLocalStorage.email;
-    formMessegeEl.value = parsedGetValueLocalStorage.message;
-  }
-}
+  const getValueLocalStorage = JSON.parse(localStorage.getItem(FORM_KEY)) ?? {};
+  const inputNames = Object.keys(getValueLocalStorage);
+  inputNames.forEach(inputName => {
+    let input = formEl.elements[inputName];
+    let valueKey = 'value';
+    input[valueKey] = getValueLocalStorage[inputName];
+  });
+  // const parsedGetValueLocalStorage = JSON.parse(getValueLocalStorage);
+  // if (parsedGetValueLocalStorage) {
+  //   formEmailEl.value = parsedGetValueLocalStorage.email;
+  //   formMessegeEl.value = parsedGetValueLocalStorage.message;
+  // }
+};
 onFormFillingGet();
